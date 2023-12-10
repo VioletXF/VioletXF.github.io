7ad0f607dcd92dd3f00c620bdea3b1e1ad482dea
VioletXF <zudevxf@gmail.com>
2023-12-11 01:55:32 +0900

    Config deploy

--- blog/src/components/LanguageSwitcher.tsx
+++ blog/src/components/LanguageSwitcher.tsx
@@ -1,11 +1,12 @@
 import { Link } from "gatsby";
 
 export function LanguageSwitcher({translations = ["en", "ko"]}: {translations?: string[]}) {
+  const pathname = typeof window !== "undefined" ? window.location.pathname : ""
   const LangMap: Record<string, string> = {
     "en": "English",
     "ko": "한국어",
   };
-  const pathWithoutLang = location.pathname.replace(/^\/.*?\//, ``);
+  const pathWithoutLang = pathname.replace(/^\/.*?\//, ``);
   if(translations.length === 1) {
     return <></>;
   }
